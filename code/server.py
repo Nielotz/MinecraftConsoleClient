88945c80f869c0adff163f1247d6300bd4fb5f68
Nielotz <mati2806@poczta.fm>
2020-04-29 16:14:07 +0200

    Move static method out of class

--- code/server.py
+++ code/server.py
@@ -9,37 +9,7 @@ from connection import Connection
 from hash_tables import ProtocolVersion
 
 
-class Server:
-    socket_data = None
-    connection: Connection = None
-
-    def __init__(self, host, port):
-        # TODO change params to class IP with ip parser, etc.
-
-        logging.info(f"Server address: '{host}:{port}'")
-
-        self.socket_data = (host, port)
-        self.connection = Connection()
-
-    def connect(self, timeout=5):
-        logging.info(f"Connecting to: "
-                     f"'{self.socket_data[0]}:{self.socket_data[1]}'")
-        try:
-            self.connection.connect(self.socket_data, timeout)  # Status request.
-        except OSError as e:
-            logging.error(f"Can't connect to: "
-                          f"'{self.socket_data[0]}:{self.socket_data[1]}'"
-                          f", reason: {e}")
-            return False
-
-        logging.info("Connected")
-        return True
-
-    def __del__(self):
-        pass
-
-    @staticmethod
-    def status(address, port):
+def get_status(address, port):
         """
         Create socket, connect to server, request for information.
         On success return json, on error False
@@ -60,7 +30,7 @@ class Server:
             logging.error(f"Can't connect to: '{address}:{port}', reason: {e}")
             return False
         else:
-            logging.info(f"Connected'")
+            logging.info(f"Connected")
 
         connection.send(b'\x00\x00',  # Protocol Version
                         address,  # Server Address
@@ -91,4 +61,36 @@ class Server:
             pass
 
         return response
+
+
+class Server:
+    socket_data = None
+    connection: Connection = None
+
+    def __init__(self, host, port):
+        # TODO change params to class IP with ip parser, etc.
+
+        logging.info(f"Server address: '{host}:{port}'")
+
+        self.socket_data = (host, port)
+        self.connection = Connection()
+
+    def connect(self, timeout=5):
+        logging.info(f"Connecting to: "
+                     f"'{self.socket_data[0]}:{self.socket_data[1]}'")
+        try:
+            self.connection.connect(self.socket_data, timeout)  # Status request.
+        except OSError as e:
+            logging.error(f"Can't connect to: "
+                          f"'{self.socket_data[0]}:{self.socket_data[1]}'"
+                          f", reason: {e}")
+            return False
+
+        logging.info("Connected")
+        return True
+
+    def __del__(self):
+        pass
+
+
 
