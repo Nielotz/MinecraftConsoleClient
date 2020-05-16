d1597c54bb8a15be887bf2d9185e7b9bc4873a24
Nielotz <mati2806@poczta.fm>
2020-05-16 16:00:14 +0200

    Add health

--- minecraft_console_client/data_structures/mob.py
+++ minecraft_console_client/data_structures/mob.py
@@ -1,8 +1,9 @@
 class Mob:
-    username: str = "Anyone"
     entity_id: int = None
     pos_x: int = None
     pos_y: int = None
     pos_z: int = None
     yaw: int = None
     pitch: int = None
+    health: float = None
+
