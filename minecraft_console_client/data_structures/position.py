cbe94db6ff35598f939d8d2056d0fef0c8d70ffb
Nielotz <mati2806@poczta.fm>
2020-06-03 14:54:26 +0200

    Remove look from position, add get

--- minecraft_console_client/data_structures/position.py
+++ minecraft_console_client/data_structures/position.py
@@ -1,18 +1,20 @@
+from typing import Union
+
+
 class Position:
     x = None
     y = None
     z = None
-    yaw = None
-    pitch = None
 
-    def __init__(self, pos: (int, int, int), yaw=None, pitch=None):
+    def __init__(self, pos: (int, int, int)):
         """ pos = (x, y, z)"""
         self.x = pos[0]
         self.y = pos[1]
         self.z = pos[2]
-        self.yaw = yaw
-        self.pitch = pitch
+
+    def get(self):
+        """ Returns tuple (x, y, z)"""
+        return self.x, self.y, self.z
 
     def __repr__(self):
-        return f"[x: {self.x}, y: {self.y}, z: {self.z}, " \
-               f"yaw: {self.yaw}, pitch: {self.pitch}]"
+        return f"[x: {self.x}, y: {self.y}, z: {self.z}]"
