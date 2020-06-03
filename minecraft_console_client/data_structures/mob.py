7c55941321f7f29e7cc985de4d96fb8120d5cb46
Nielotz <mati2806@poczta.fm>
2020-06-03 14:53:48 +0200

    Add yaw, pitch, velocity

--- minecraft_console_client/data_structures/mob.py
+++ minecraft_console_client/data_structures/mob.py
@@ -4,6 +4,11 @@ from data_structures.position import Position
 class Mob:
     entity_id: int = None
     position: Position = None
+    look_yaw: float = 0
+    look_pitch: float = 0
     health: float = None
     on_ground: bool = None
+    velocity_x: int = None
+    velocity_y: int = None
+    velocity_z: int = None
 
