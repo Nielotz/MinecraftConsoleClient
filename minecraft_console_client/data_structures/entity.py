2b466fe563abce3c960be08a19d8e41e6b661068
Nielotz <mati2806@poczta.fm>
2020-06-28 16:59:33 +0200

    Change default values to None

--- minecraft_console_client/data_structures/entity.py
+++ minecraft_console_client/data_structures/entity.py
@@ -4,8 +4,8 @@ from data_structures.position import Position
 class Entity:
     entity_id: int = None
     position: Position = None
-    look_yaw: float = 0
-    look_pitch: float = 0
+    look_yaw: float = None
+    look_pitch: float = None
     health: float = None
     on_ground: bool = None
     velocity: [int, int, int] = [None, None, None]  # (x, y, z)
