1edb786ebe00645e3896a046de95e473749196ba
Nielotz <mati2806@poczta.fm>
2021-07-04 23:03:39 +0200

    Reformat file

--- minecraft_console_client/misc/converters.py
+++ minecraft_console_client/misc/converters.py
@@ -48,6 +48,239 @@ def convert_to_varint(value: int) -> bytes:
     return bytes(varint)
 
 
+def pack_float(value: float) -> bytes:
+    """
+    Pack float into 4 bytes and return them.
+
+    :param value: value to be packed
+    :return: bytes of packed float
+    :rtype: bytes
+    """
+    return struct.pack("!f", value)
+
+
+def pack_double(value: float) -> bytes:
+    """
+    Pack float into 8 bytes and return them.
+
+    :param value: value to be packed
+    :return: bytes of packed float
+    :rtype: bytes
+    """
+    return struct.pack("!d", value)
+
+
+def pack_unsigned_short(value: int) -> bytes:
+    """
+    Pack unsigned short into 2 bytes and return them.
+
+    :param value: value to be packed
+    :return: bytes of packed value
+    :rtype: bytes
+    """
+    return struct.pack("!H", value)
+
+
+def pack_bool(value: bool) -> bytes:
+    """
+    Pack value into 1 byte and return it.
+
+    :param value: value to be packed
+    :return: bytes of packed float
+    :rtype: bytes
+    """
+    if value:
+        return b'\x01'
+    return b'\x00'
+
+
+def pack_byte(value) -> bytes:
+    """
+    Pack value into 1 byte and return it.
+
+    :param value: value to be packed
+    :return: bytes of packed float
+    :rtype: bytes
+    """
+    return struct.pack("!b", value)
+
+
+def pack_long(value: float) -> bytes:
+    """
+    Pack long into 8 bytes and return them.
+
+    :param value: value to be packed
+    :return: bytes of packed long
+    :rtype: bytes
+    """
+    return struct.pack("!q", value)
+
+
+def pack_string(value: str) -> bytes:
+    """
+    Prefix value with its length as varint.
+
+    :param value: value to be packed
+    :return: bytes of packed string
+    :rtype: bytes
+    """
+    value = value.encode("utf-8")
+    return b''.join((convert_to_varint(len(value)), value))
+
+
+def extract_int(data: bytes) -> (int, Union[bytes, None]):
+    """
+    Extract int from bytes.
+
+    :param data: at least 4 bytes from which extract int
+    :return extracted int, leftover of data or None
+    :rtype int, Union[bytes, None]
+    """
+    value = int.from_bytes(data[:4:], byteorder="big", signed=True)
+
+    if len(data) > 4:
+        return value, data[4:]
+    return value, None
+
+
+def extract_boolean(data: bytes) -> (bool, Union[bytes, None]):
+    """
+    Extract boolean from bytes.
+
+    :param data: at least 1 byte from which extract
+    :return True or False, leftover of data or None
+    :rtype bool, Union[bytes, None]
+    """
+    if len(data) > 1:
+        return data[0] and True, data[1:]
+    return data[0] and True, None
+
+
+def extract_byte(data: bytes) -> (int, Union[bytes, None]):
+    """
+    Extract byte from bytes.
+
+    :param data: at least 1 byte from which extract
+    :return byte, leftover of data or None
+    :rtype byte, Union[bytes, None]
+    """
+    # int.from_bytes(data[:1:], byteorder="big", signed=True)
+
+    # If negative subtract 256
+    if len(data) > 1:
+        return data[0] - ((data[0] & 0x80) << 1), data[1:]
+    return data[0] - ((data[0] & 0x80) << 1), None
+
+
+def extract_unsigned_byte(data: bytes) -> (int, Union[bytes, None]):
+    """
+    Extract unsigned byte from bytes.
+
+    :param data: at least 1 byte from which extract byte
+    :return extracted unsigned byte, leftover of data or None
+    :rtype int, Union[bytes, None]
+    """
+    if len(data) > 1:
+        return data[0], data[1:]
+    return data[0], None
+
+
+def extract_long(data: bytes) -> (int, Union[bytes, None]):
+    """
+    Extract long from bytes.
+
+    :param data: at least 8 bytes from which extract int
+    :return extracted int, leftover of data or None
+    :rtype int, Union[bytes, None]
+    """
+    value = int.from_bytes(data[:8:], byteorder="big", signed=True)
+
+    if len(data) > 8:
+        return value, data[8:]
+    return value, None
+
+
+def extract_float(data: bytes) -> (float, Union[bytes, None]):
+    """
+    Extract float from bytes.
+
+    :param data: at least 4 bytes from which extract float
+    :return extracted float, leftover of data or None
+    :rtype float, Union[bytes, None]
+    """
+    value: float = struct.unpack('>f', data[:4:])[0]
+    if len(data) > 4:
+        return value, data[4:]
+    return value, None
+
+
+def extract_double(data: bytes) -> (float, Union[bytes, None]):
+    """
+    Extract double from bytes.
+
+    :param data: at least 8 bytes from which extract double
+    :return extracted double, leftover of data or None
+    :rtype float, Union[bytes, None]
+    """
+    value: float = struct.unpack('>d', data[:8:])[0]
+    if len(data) > 8:
+        return value, data[8:]
+    return value, None
+
+
+def extract_string(data: bytes) -> (bytes, Union[bytes, None]):
+    """
+    Extract string from given bytes.
+
+    :param data: decompressed array of bytes
+    :return bytes of unicode string, leftover of data or None
+    :rtype bytes, Union[bytes, None]
+    """
+    string_len, data = extract_varint_as_int(data)
+    if len(data) > string_len:
+        return data[:string_len], data[string_len:]
+    return data[:string_len], None
+
+
+def extract_position(data: bytes) -> (Position, Union[bytes, None]):
+    """
+    Extract Position(x, y, z) from data.
+
+    :param data: at least 8 bytes from which extract position
+    :returns extracted position, leftover or None when no data left
+    :rtype Position, Union[bytes, None]
+    """
+    # TODO: replace from_bytes with struct.. or another way.
+    val = int.from_bytes(data[:8], byteorder="big", signed=True)
+
+    z = val & 0x3ffffff
+    val >>= 26
+    y = val & 0xfff
+    x = val >> 12
+
+    if z >= 0x2000000:  # 2 ** 25
+        z -= 0x4000000  # 2 ** 26
+
+    position = Position(x, y, z)
+
+    if len(data) > 8:
+        return position, data[8:]
+    return position, None
+
+
+def extract_json_from_chat(data: bytes) -> (dict, Union[bytes, None]):
+    """
+    Extract json from the chat.
+
+    :param data: bytes from which to extract json
+    :return extracted json, leftover of data or None
+    :rtype int, Union[bytes, None]
+    """
+    # TODO: return python JSON
+    string, leftover = extract_string(data)
+    return json.loads(string), leftover
+
+
 def extract_varint_as_int(data: bytes) -> (int, bytes):
     """
     Extract varint from uncompressed data and returns it as int.
@@ -95,239 +328,3 @@ def extract_packet_data(data: bytes, compression=False) -> (int, bytes):
 
     return packet_id, data
 
-
-def pack_float(value: float) -> bytes:
-    """
-    Pack float into 4 bytes and return them.
-
-    :param value: value to be packed
-    :return: bytes of packed float
-    :rtype: bytes
-    """
-    return struct.pack("!f", value)
-
-
-def pack_double(value: float) -> bytes:
-    """
-    Pack float into 8 bytes and return them.
-
-    :param value: value to be packed
-    :return: bytes of packed float
-    :rtype: bytes
-    """
-    return struct.pack("!d", value)
-
-
-def pack_unsigned_short(value: int) -> bytes:
-    """
-    Pack unsigned short into 2 bytes and return them.
-
-    :param value: value to be packed
-    :return: bytes of packed value
-    :rtype: bytes
-    """
-    return struct.pack("!H", value)
-
-
-def pack_long(value: float) -> bytes:
-    """
-    Pack long into 8 bytes and return them.
-
-    :param value: value to be packed
-    :return: bytes of packed long
-    :rtype: bytes
-    """
-    return struct.pack("!q", value)
-
-
-def pack_byte(value) -> bytes:
-    """
-    Pack value into 1 byte and return it.
-
-    :param value: value to be packed
-    :return: bytes of packed float
-    :rtype: bytes
-    """
-    return struct.pack("!b", value)
-
-
-def pack_bool(value: bool) -> bytes:
-    """
-    Pack value into 1 byte and return it.
-
-    :param value: value to be packed
-    :return: bytes of packed float
-    :rtype: bytes
-    """
-    if value:
-        return b'\x01'
-    return b'\x00'
-
-
-def pack_string(value: str) -> bytes:
-    """
-    Prefix value with its length as varint.
-
-    :param value: value to be packed
-    :return: bytes of packed string
-    :rtype: bytes
-    """
-    value = value.encode("utf-8")
-    return b''.join((convert_to_varint(len(value)), value))
-
-
-def extract_string(data: bytes) -> (bytes, Union[bytes, None]):
-    """
-    Extract string from given bytes.
-
-    :param data: decompressed array of bytes
-    :return bytes of unicode string, leftover of data or None
-    :rtype bytes, Union[bytes, None]
-    """
-    string_len, data = extract_varint_as_int(data)
-    string = data[:string_len:]
-
-    if len(data) > string_len:
-        data = data[string_len:]
-        return string, data
-
-    return string, None
-
-
-def extract_json_from_chat(data: bytes) -> (dict, Union[bytes, None]):
-    """
-    Extract json from the chat.
-
-    :param data: bytes from which to extract json
-    :return extracted json, leftover of data or None
-    :rtype int, Union[bytes, None]
-    """
-    # TODO: return python JSON
-    string, leftover = extract_string(data)
-    return json.loads(string), leftover
-
-
-def extract_int(data: bytes) -> (int, Union[bytes, None]):
-    """
-    Extract int from bytes.
-
-    :param data: at least 4 bytes from which extract int
-    :return extracted int, leftover of data or None
-    :rtype int, Union[bytes, None]
-    """
-    value = int.from_bytes(data[:4:], byteorder="big", signed=True)
-
-    if len(data) > 4:
-        return value, data[4:]
-    return value, None
-
-
-def extract_unsigned_byte(data: bytes) -> (int, Union[bytes, None]):
-    """
-    Extract unsigned byte from bytes.
-
-    :param data: at least 1 byte from which extract byte
-    :return extracted unsigned byte, leftover of data or None
-    :rtype int, Union[bytes, None]
-    """
-    if len(data) > 1:
-        return data[0], data[1:]
-    return data[0], None
-
-
-def extract_boolean(data: bytes) -> (bool, Union[bytes, None]):
-    """
-    Extract boolean from bytes.
-
-    :param data: at least 1 byte from which extract
-    :return True or False, leftover of data or None
-    :rtype bool, Union[bytes, None]
-    """
-    if len(data) > 1:
-        return data[0] and True, data[1:]
-    return data[0] and True, None
-
-
-def extract_byte(data: bytes) -> (int, Union[bytes, None]):
-    """
-    Extract byte from bytes.
-
-    :param data: at least 1 byte from which extract
-    :return byte, leftover of data or None
-    :rtype byte, Union[bytes, None]
-    """
-    # int.from_bytes(data[:1:], byteorder="big", signed=True)
-
-    # If negative subtract 256
-    if len(data) > 1:
-        return data[0] - ((data[0] & 0x80) << 1), data[1:]
-    return data[0] - ((data[0] & 0x80) << 1), None
-
-
-def extract_float(data: bytes) -> (float, Union[bytes, None]):
-    """
-    Extract float from bytes.
-
-    :param data: at least 4 bytes from which extract float
-    :return extracted float, leftover of data or None
-    :rtype float, Union[bytes, None]
-    """
-    value: float = struct.unpack('>f', data[:4:])[0]
-    if len(data) > 4:
-        return value, data[4:]
-    return value, None
-
-
-def extract_double(data: bytes) -> (float, Union[bytes, None]):
-    """
-    Extract double from bytes.
-
-    :param data: at least 8 bytes from which extract double
-    :return extracted double, leftover of data or None
-    :rtype float, Union[bytes, None]
-    """
-    value: float = struct.unpack('>d', data[:8:])[0]
-    if len(data) > 8:
-        return value, data[8:]
-    return value, None
-
-
-def extract_position(data: bytes) -> (Position, Union[bytes, None]):
-    """
-    Extract Position(x, y, z) from data.
-
-    :param data: at least 8 bytes from which extract position
-    :returns extracted position, leftover or None when no data left
-    :rtype Position, Union[bytes, None]
-    """
-    # TODO: replace from_bytes with struct.. or another way.
-    val = int.from_bytes(data[:8], byteorder="big", signed=True)
-
-    z = val & 0x3ffffff
-    val >>= 26
-    y = val & 0xfff
-    x = val >> 12
-
-    if z >= 0x2000000:  # 2 ** 25
-        z -= 0x4000000  # 2 ** 26
-
-    position = Position(x, y, z)
-
-    if len(data) > 8:
-        return position, data[8:]
-    return position, None
-
-
-def extract_long(data: bytes) -> (int, Union[bytes, None]):
-    """
-    Extract long from bytes.
-
-    :param data: at least 8 bytes from which extract int
-    :return extracted int, leftover of data or None
-    :rtype int, Union[bytes, None]
-    """
-    value = int.from_bytes(data[:8:], byteorder="big", signed=True)
-
-    if len(data) > 8:
-        return value, data[8:]
-    return value, None
