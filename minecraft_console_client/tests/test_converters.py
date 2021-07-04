1bb5639a38d19f1e9f6eae91ce5bb6007b358a58
Nielotz <mati2806@poczta.fm>
2021-07-04 23:04:02 +0200

    Add test for string, unify tests.

--- minecraft_console_client/tests/test_converters.py
+++ minecraft_console_client/tests/test_converters.py
@@ -1,5 +1,7 @@
 import time
 from random import randint, uniform
+import random
+import string
 from unittest import TestCase
 
 from misc.consts import *
@@ -8,31 +10,49 @@ from misc.converters import *
 from fractions import Fraction
 
 
-class Test(TestCase):
+def to_fraction(data):
+    return Fraction(struct.unpack('f', struct.pack('f', data))[0])
 
-    def _test(self, to_varint_converter, from_varint_converter,
-              dataset=(), test_desc: str = "", is_float: bool = False,
-              is_double: bool = False):
+
+class Test(TestCase):
+    def _test(self, converter,
+              deconverter,
+              dataset=(),
+              test_desc: str = "",
+              is_float: bool = False, is_double: bool = False,
+              is_string: bool = False):
         # Config
+        n_of_repeats = 10  # How many times test to get best time.
+
+        time_ns = time.perf_counter_ns
         times = {"pack": [], "extract": []}
         for data in dataset:
             with self.subTest(test_desc):
-                start = time.perf_counter_ns()
-                varint = to_varint_converter(data)
-                times["pack"].append(time.perf_counter_ns() - start)
+                pack_time, unpack_time = 100000000, 1000000000
+                for _ in range(n_of_repeats):
+                    start = time_ns()
+                    converted = converter(data)
+                    if (_pack_time := (time_ns() - start)) < pack_time:
+                        pack_time = _pack_time
 
-                start = time.perf_counter_ns()
-                decoded_varint, _ = from_varint_converter(varint)
-                times["extract"].append(time.perf_counter_ns() - start)
+                    start = time_ns()
+                    deconverted, _ = deconverter(converted)
+                    if (_unpack_time := (time_ns() - start)) < unpack_time:
+                        unpack_time = _unpack_time
 
-                if is_float:
-                    data = Fraction(struct.unpack('f', struct.pack('f', data))[0])
-                    decoded_varint = Fraction(struct.unpack('f', struct.pack('f', decoded_varint))[0])
-                    self.assertEqual(data, decoded_varint)
-                elif is_double:
-                    self.assertEqual(data, decoded_varint)
-                else:
-                    self.assertEqual(data, decoded_varint)
+                    if is_float:
+                        self.assertEqual(
+                            to_fraction(data),
+                            to_fraction(deconverted))
+                    elif is_double:
+                        self.assertEqual(data, deconverted)
+                    elif is_string:
+                        self.assertEqual(data.encode("utf-8"), deconverted)
+                    else:
+                        self.assertEqual(data, deconverted)
+                times["pack"].append(pack_time)
+                times["extract"].append(unpack_time)
+
         total_pack_time = sum(times["pack"])
         total_extract_time = sum(times["extract"])
         print(f"""TEST: {test_desc}
@@ -46,60 +66,63 @@ class Test(TestCase):
 
     def test_converters(self):
         # Config.
-        n_of_numbers: int = 10000
-        numbers: [int, ] = [0] * n_of_numbers
-        # Integers.
-        for i in range(n_of_numbers):
-            numbers[i] = randint(MIN_INT, MAX_INT)
-        edge = [0, 1, 254, 255, 265, 2**16-1, 2**16, 2**16+1,
-                -1, -254, -255, -265, -2**16+1, -2**16, -2**16-1,
-                MAX_INT-1, MAX_INT, MIN_INT + 1, MIN_INT,
-                ]
-        self._test(convert_to_varint, extract_varint_as_int, numbers + edge,
-                   "Converters - varint")
-        # self._test(_, extract_int, numbers,
-        #                    "Converters - varint")
+        n_of_strings: int = 1000
+        n_of_letters: int = 100
+        n_of_numbers: int = 1000
 
-        for i in range(n_of_numbers // 2, n_of_numbers, 2):
-            numbers[i] = randint(MAX_INT, MAX_LONG)
-            numbers[i + 1] = randint(MIN_LONG, MIN_INT)
-        edge += [MAX_LONG, MAX_LONG - 1, MIN_LONG, MIN_LONG + 1]
-        self._test(pack_long, extract_long, numbers+edge, "Converters - long")
-
-        for i in range(n_of_numbers):
-            numbers[i] = randint(MIN_BYTE, MAX_BYTE)
-        edge = [MIN_BYTE, MIN_BYTE + 1, 0, MAX_BYTE - 1, MAX_BYTE]
-        self._test(pack_byte, extract_byte, numbers+edge, "Converters - byte")
-        # self._test(_, extract_unsigned_byte, numbers,
-        #            "Converters - byte")
-        # self._test(pack_unsigned_short, _, numbers,
-        #            "Converters - byte")
-
-        # Adjust strings based on real data.
-        # for i in range(n_of_numbers):
-        #     numbers[i] = randint(MIN_BYTE, MAX_BYTE)
-        # self._test(pack_string, extract_string, numbers, "Converters - byte")
-
-        # Float-points.
-        numbers = [0.] * n_of_numbers
-        mx = MAX_BYTE - 1  # Right limit for uniform.
-        for i in range(n_of_numbers):
-            numbers[i] = uniform(MIN_BYTE, mx)
-        edge = [MIN_FLOAT, MIN_FLOAT+1, MAX_FLOAT-1, MAX_FLOAT]
-        self._test(pack_float, extract_float, numbers+edge,
-                   "Converters - float", is_float=True)
-
-        mx = MAX_BYTE - 1  # Right limit for uniform.
-        for i in range(n_of_numbers):
-            numbers[i] = uniform(MIN_BYTE, mx)
-        edge += [MIN_DOUBLE, MIN_DOUBLE+1, MAX_DOUBLE-1, MAX_DOUBLE]
-        self._test(pack_double, extract_double, numbers+edge,
-                   "Converters - double", is_double=True)
-
-        # Exclusives.
+        # bool
         edge = [True, False]
         self._test(pack_bool, extract_boolean, edge, "Converters - bool")
 
-        # extract_data()
-        # extract_json_from_chat()
-        # extract_position()
+        # byte
+        numbers = [randint(MIN_BYTE, MAX_BYTE) for _ in range(n_of_numbers)]
+        edge = [MIN_BYTE, MIN_BYTE + 1, 0, MAX_BYTE - 1, MAX_BYTE]
+        self._test(pack_byte, extract_byte, numbers + edge, "Converters - byte")
+
+        # unsigned byte
+
+        # unsigned short
+
+        # int
+
+        # long
+        numbers = [randint(MIN_LONG, MAX_LONG) for _ in range(n_of_numbers)]
+        edge += [MAX_LONG, MAX_LONG - 1, MIN_LONG, MIN_LONG + 1]
+        self._test(pack_long, extract_long, numbers + edge,
+                   "Converters - long")
+
+        # float
+        numbers = [uniform(MIN_FLOAT, MAX_FLOAT) for _ in range(n_of_numbers)]
+        edge = [MIN_FLOAT, MIN_FLOAT + 1, MAX_FLOAT - 1, MAX_FLOAT]
+        self._test(pack_float, extract_float, numbers + edge,
+                   "Converters - float", is_float=True)
+
+        # double
+        numbers = [uniform(MIN_DOUBLE, MAX_DOUBLE) for _ in range(n_of_numbers)]
+        edge += [MIN_DOUBLE, MIN_DOUBLE + 1, MAX_DOUBLE - 1, MAX_DOUBLE]
+        self._test(pack_double, extract_double, numbers + edge,
+                   "Converters - double", is_double=True)
+
+        # varint
+        numbers = [randint(MIN_INT, MAX_INT) for _ in range(n_of_numbers)]
+        edge = [0, 1, 254, 255, 265,
+                -1, -254, -255, -265,
+                2 ** 16 - 1, 2 ** 16, 2 ** 16 + 1,
+                -2 ** 16 + 1, -2 ** 16, -2 ** 16 - 1,
+                MAX_INT - 1, MAX_INT, MIN_INT + 1, MIN_INT,
+                ]
+        self._test(convert_to_varint, extract_varint_as_int, numbers + edge,
+                   "Converters - varint")
+
+        # string
+        letters = string.printable
+        strings = [''.join(random.choice(letters) for _ in range(n_of_letters))
+                   for _ in range(n_of_strings)]
+        edge = []
+        self._test(pack_string, extract_string, strings + edge,
+                   "Converters - string", is_string=True)
+        # extract position
+
+        # extract json from chat
+
+        # extract packet data
