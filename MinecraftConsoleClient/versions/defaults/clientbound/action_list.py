fcc87663658f81b3d7f17c8e5782bc3ba9988fce
Nielotz <mati2806@poczta.fm>
2021-08-16 19:15:41 +0200

    Refactor

--- MinecraftConsoleClient/versions/defaults/clientbound/action_list.py
+++ MinecraftConsoleClient/versions/defaults/clientbound/action_list.py
@@ -1,13 +1,13 @@
-"""Provide action_list containing references to functions to run when \
-received given packet ID."""
+"""Provide action_list containing references to functions to run when received given packet ID."""
 
-# Sample:
 action_list = {
     "status": {
-        0x00: print,
-        1: print
     },
-    "login": {
+    "login": {  # Sample based on 1.12.2:
+        # 0: login.disconnect,
+        # 1: Clientbound._encryption_request,
+        # 2: login.login_success,
+        # 3: login.set_compression,
     },
     "play": {
     }
--- MinecraftConsoleClient/versions/v1_12_2/__init__.py
+++ MinecraftConsoleClient/versions/v1_12_2/__init__.py
@@ -2,14 +2,15 @@
 import types
 
 import versions.defaults
+from misc.converters import convert_to_varint
 
 
 class VersionData(versions.defaults.VersionData):
     """Hold version data."""
 
-    release_name = "1.12.2"
-    protocol_version_number = 340
-    protocol_version_varint = b'\xd4\x02'  # Can be calculated using utils
+    release_name: str = "1.12.2"
+    protocol_version_number: int = 340
+    protocol_version_varint: bytes = convert_to_varint(340)  # b'\xd4\x02'
 
     from versions.v1_12_2.data_structures.game_data import GameData
     game_data: GameData = GameData()
