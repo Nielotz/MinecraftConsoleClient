e150d156817d834951c43cdedf72592f3689aacc
Nielotz <mati2806@poczta.fm>
2021-08-17 22:24:04 +0200

    Remove old coments.

--- MinecraftConsoleClient/versions/v1_12_2/data_structures/world_data.py
+++ MinecraftConsoleClient/versions/v1_12_2/data_structures/world_data.py
@@ -1,16 +1,13 @@
-"""Holder for GameData."""
 from versions.defaults.data_structures.world_data import WorldData as BaseWorldData
 
 from versions.v1_12_2.data_structures.world.world import World
 
 
 class WorldData(BaseWorldData):
-    """Contain data related to the game."""
     world: World = None
 
     def __init__(self):
         self._init_world()
 
     def _init_world(self):
-        """Initialize world data."""
         self.world = World()
