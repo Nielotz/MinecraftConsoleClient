0addc20fc9afcf19ab5541e8057e0e32d4de1b13
Nielotz <mati2806@poczta.fm>
2021-08-22 19:19:05 +0200

    v0.4.1

---
+++ tests/performance/analyze.py
@@ -0,0 +1,41 @@
+from pyinstrument import Profiler
+profiler = Profiler()
+profiler.start()
+import logging
+
+# TODO: improve logger.
+logger = logging.getLogger("mainLogger")
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# fh = logging.FileHandler()
+# fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+from game import Game
+from data_structures.host import Host
+from data_structures.player import Player
+from versions.version import Version
+
+if __name__ == "__main__":
+
+    host: Host = Host("192.168.56.1", 25565)
+    # host: Host = Host("188.68.236.16", 25565)
+    # host: Host = Host("89.22.210.172", 25565) konrad
+    # host: Host = Host("185.243.53.224", 25577)
+    # host: Host = Host("nssv.pl", 25565)
+
+    player: Player = Player(username="Bob")
+
+    game_version: Version = Version.v1_12_2
+
+    game = Game(host, player, game_version)
+    try:
+        game.start()
+    except Exception:
+        pass
+profiler.stop()
+
+profiler.print()
