a6739ff97566dd43a140514f57bc965b8ecece9e
Nielotz <mati2806@poczta.fm>
2021-09-05 22:37:30 +0200

    Split Entity, add init

--- MinecraftConsoleClient/data_structures/entity.py
+++ MinecraftConsoleClient/data_structures/entity.py
@@ -1,15 +1,34 @@
-"""Holder for Entity."""
-
 from data_structures.position import Position
 
 
-class Entity:
-    """Contain data related to entity."""
+class Look:
+    def __init__(self,
+                 yaw: float = None,
+                 pitch: float = None):
+        self.yaw: float = yaw
+        self.pitch: float = pitch
 
-    entity_id: int = None
-    position: Position = None
-    look_yaw: float = None
-    look_pitch: float = None
-    health: float = None
-    on_ground: bool = None
-    velocity: [int, int, int] = [None, None, None]  # (x, y, z)
+
+class Velocity:
+    def __init__(self,
+                 x: int = 0,
+                 y: int = 0,
+                 z: int = 0):
+        self.x: int = x
+        self.y: int = y
+        self.z: int = z
+
+
+class Entity:
+    def __init__(self, id_: int = None,
+                 position: Position = None,
+                 look: Look = Look(),
+                 health: float = None,
+                 on_ground: bool = None,
+                 velocity: Velocity = Velocity()):
+        self.id_: int = id_
+        self.position: Position = position
+        self.look: Look = look
+        self.health: float = health
+        self.on_ground: bool = on_ground
+        self.velocity: Velocity = velocity
