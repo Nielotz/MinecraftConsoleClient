575d5a12756b3ddb03a9cd4998bff77f79340b80
Nielotz <mati2806@poczta.fm>
2021-09-10 21:48:11 +0200

    Remove old code leftover

--- MinecraftConsoleClient/__init__.py
+++ MinecraftConsoleClient/__init__.py
@@ -2,7 +2,8 @@ if __name__ == "__main__":
 
     from data_structures.host import Host
 
-    host: Host = Host("192.168.56.1", 25565)
+    host: Host = Host("127.0.0.1", 25565)
+    # host: Host = Host("192.168.56.1", 25565)
     # host: Host = Host("188.68.236.16", 25565)
     # host: Host = Host("89.22.210.172", 25565) konrad
     # host: Host = Host("185.243.53.224", 25577)
@@ -21,7 +22,7 @@ if __name__ == "__main__":
     from game import Game
 
     # Start game.
-    game = Game(host, hero, game_version)
+    game = Game(host, hero)
 
     # TODO: rewrite:
     # Check exited with error.
