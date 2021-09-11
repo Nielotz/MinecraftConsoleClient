0bd5f15dc02f03d0fd167ca0faff15cfa3b55029
Nielotz <mati2806@poczta.fm>
2021-09-11 20:08:38 +0200

    Rename Player to Hero, update Game usage.

--- tests/performance/analyze.py
+++ tests/performance/analyze.py
@@ -1,7 +1,7 @@
 from pyinstrument import Profiler
 
 from data_structures.host import Host
-from data_structures.player import Player_
+from data_structures.hero import Hero
 from game import Game
 from misc.logger import get_logger
 from versions.version import VersionVersion
@@ -13,17 +13,13 @@ logger = get_logger("mainLogger")
 
 if __name__ == "__main__":
 
-    host: Host = Host("192.168.56.1", 25565)
-    # host: Host = Host("188.68.236.16", 25565)
-    # host: Host = Host("89.22.210.172", 25565) konrad
-    # host: Host = Host("185.243.53.224", 25577)
-    # host: Host = Host("nssv.pl", 25565)
+    host: Host = Host("127.0.0.1", 25565)
 
-    player: Player_ = Player_(username="Bob")
+    hero: Hero = Hero(username="Bob")
 
     game_version: VersionVersion = VersionVersion.V1_12_2
 
-    game = Game(host, player, game_version)
+    game = Game(host, hero)
     try:
         game.start()
     except Exception:
